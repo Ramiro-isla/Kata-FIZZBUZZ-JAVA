8632a756d48361d78b440e07068138a15d5dff32
Ramiro-isla <ramiroisla11@gmail.com>
2023-02-12 21:22:26 +0100

    kata done

--- src/main/java/com/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/fizzbuzz/FizzBuzz.java
@@ -1,5 +1,18 @@
 package com.fizzbuzz;
 
 public class FizzBuzz {
-    
+
+    public String fizzbuzz(int numb) {
+        if (numb % 3 == 0 && numb % 5 != 0 || String.valueOf(numb).contains("3")) {
+            return "Fizz";
+        }
+        if (numb % 5 == 0 && numb % 3 != 0 || String.valueOf(numb).contains("5")) {
+            return "Buzz";
+        }
+        if (numb % 5 == 0 && numb % 3 == 0) {
+            return "FizzBuzz";
+        }
+        return "Error";
+
+    }
 }
